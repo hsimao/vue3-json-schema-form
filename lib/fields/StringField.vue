0e121f4504b28c8a1f488187700527c8e6cb14b7
hsimao <e087754958@gmail.com>
2020-11-02 20:47:58 +0800

    add: StringFiled tsx change to .vue component

---
+++ lib/fields/StringField.vue
@@ -0,0 +1,22 @@
+<template>
+  <input type="text" :value="value" @input="handleChagne" />
+</template>
+
+<script lang="ts" setup="props">
+import { FiledPropsDefine, Schema } from '../types'
+
+export default {
+  props: FiledPropsDefine
+}
+
+declare const props: {
+  value: any
+  onChange: (v: string) => void
+  schame: Schema
+}
+
+export const handleChagne = (e: any) => {
+  console.log(e)
+  props.onChange(e.target.value)
+}
+</script>
